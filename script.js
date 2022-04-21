c2284776f1f4860738f954d3b8b050a91ecd83e1
AtosHackerMan <atosfalcon@gmail.com>
2022-04-21 10:13:53 -0500

    debugged final issues

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-var principal = document.getElementById("principal").value
+function myFunction() {
+    alert("popo")
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value
+    document.getElementById("rate_val").innerText=rateval
+}
+
+function compute() {
+    var principal = document.getElementById("principal").value
 var rate = document.getElementById("rate").value
 var years = document.getElementById("years").value
 var interest = principal * years * rate /100
 var year = new Date().getFullYear()+parseInt(years)
-var updateRate = () => {
-    var rateval = document.getElementById("rate").value
-    document.getElementById("rate_val").innerText=rateval
-}
 var result = document.getElementById("result")
-
-function compute()
-{
     var pfocus = document.getElementById("principal")
+    p = document.getElementById("principal").value;
 
     if(principal <= 0) {
         alert("Enter a positive number")
         focus(pfocus)
         return false
     }
-
-    p = document.getElementById("principal").value;
+    
     result.innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
         
